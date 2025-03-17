603b5e438798b00b5cf664888d57f1df8c09ce7e
Michał Zając <michal.t.zajac@gmail.com>
2025-03-17 20:51:13 +0100

    Some small changes

--- src/test/scala/michalz/openquest/tools/parsers/dice/CharacteristicParserSpec.scala
+++ src/test/scala/michalz/openquest/tools/parsers/dice/CharacteristicParserSpec.scala
@@ -15,8 +15,10 @@ class CharacteristicParserSpec extends Specification with Matchers with Tables {
     s2"""
     | This is specification of CharactersitcParser
     |   It should properly parse charactersitics $characteristicTable
+    |   It should properly parse crazy charactersitics $crazyCharacteristicTable
   """.stripMargin
 
+  //format: off
   private val characteristicTable: DecoratedResult[DataTable] =
     "char string"   | "characteristic"  |>
     "3D10+12 (28)"  ! (28, "3d10+12")   |
@@ -36,5 +38,24 @@ class CharacteristicParserSpec extends Specification with Matchers with Tables {
       val result = CharacteristicParser.parseCharacteristic(char).map(c => c.base -> c.roll)
       result must beRight(expected)
     }
+  
+
+  private val crazyCharacteristicTable: DecoratedResult[DataTable] =
+    "char string"          | "characteristic"      |>
+    "3D10*(2+1d4) (42)"    ! (42, "3d10*(2+1d4)")  |
+    "(2d6+3)*4/2 (19)"     ! (19, "(2d6+3)*4/2")   |
+    "d100/2+10 (35)"       ! (35, "d100/2+10")     |
+    "2d20-1d6+4 (27)"      ! (27, "2d20-1d6+4")    |
+    "(3d6+2)*(1d4) (28)"   ! (28, "(3d6+2)*1d4")   |
+    "d100 (55)"            ! (55, "d100")          |
+    "3d6*2-5 (25)"         ! (25, "3d6*2-5")       |
+    "(d12+d8)/2+15 (22)"   ! (22, "(d12+d8)/2+15") |
+    "d20+d12+d10+d8 (41)"  ! (41, "d20+d12+d10+d8")|
+    "4d4*3-2d6 (33)"       ! (33, "4d4*3-2d6")     |
+    { (char, expected) =>
+      val result = CharacteristicParser.parseCharacteristic(char).map(c => c.base -> c.roll)
+      result must beRight(expected)
+    }
+    //format: on
 
 }
